d40d96413d941f893e12005ab79f62c6391e2f7a
Zeeshan Ahmed <zahmed@vidiumah.com>
2023-09-03 01:06:09 -0500

    Added Modal for Car Details

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -16,6 +19,22 @@ export default function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__erro-container">
+            <h2 className="text-black text-xl font-bold">
+              Oops no cars returned
+            </h2>
+          </div>
+        )}
       </div>
     </main>
   );
